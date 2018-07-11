13330ea02110c268963cb973dd8bc86dc043c36a
root <root@vps9077.inmotionhosting.com>
2018-07-11 09:42:42 -0400

    Project Upload 7-11

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ console.log('Starting app.js...');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-// const fs = require(fs);
-// const os = require(os);
-// const notes = require('./notes.js');
 
 console.log('Result:', notes.add(5, -2));
+
+//console.log(_.isString(true));
+//console.log(_.isString('Andrew'));
+
+var filteredArray = _.uniq(['Andrew', 'Andrew', 'Colton', '3', '4']);
+console.log(filteredArray);
 
 //console.log(module);
